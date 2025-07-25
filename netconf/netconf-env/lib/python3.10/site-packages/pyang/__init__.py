b47fb3e1029751216d387452bd2841373730de1e
Rilind Berbatovci <rilind.i.berbatovci@gmail.com>
2025-07-25 22:28:05 +0000

    Set up Docker GHCR publishing v.2.1.4

--- netconf/netconf-env/lib/python3.10/site-packages/pyang/__init__.py
+++
@@ -1,4 +0,0 @@
-"""The pyang library for parsing, validating, and converting YANG modules"""
-
-__version__ = '2.6.1'
-__date__ = '2024-05-23'
--- netconf/netconf-env/lib/python3.10/site-packages/pyang/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-import re
-from pyang.scripts.pyang_tool import run
-
-
-sys.exit(run())
